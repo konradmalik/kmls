b38dff8d1b80c114d0964768be372ddb95dec9db
Konrad Malik <konrad.malik@gmail.com>
2024-07-04 10:25:57 +0200

    chore: flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "kmls flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "kmls-dev";
+            packages = [ pkgs.go ];
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+    };
+}
