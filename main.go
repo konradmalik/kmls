1d1e224ff292ebe74073c034e532cb4ba2f2abd2
Konrad Malik <konrad.malik@gmail.com>
2024-07-07 11:29:58 +0200

    feat: bufio, main

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/konradmalik/kmls/rpc"
+)
 
 func main() {
-	fmt.Println("hello go")
+	logger := getLogger("/tmp/kmls.log")
+
+	logger.Println("Starting...")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(rpc.Split)
+
+	for scanner.Scan() {
+		msg := scanner.Text()
+		handleMessage(logger, msg)
+	}
+}
+
+func handleMessage(logger *log.Logger, msg any) {
+	logger.Println(msg)
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("no good file for a logger")
+	}
+
+	return log.New(logfile, "[kmls] ", log.Ldate|log.Ltime|log.Lshortfile)
 }
--- rpc/rpc.go
+++ rpc/rpc.go
@@ -8,13 +8,15 @@ import (
 	"strconv"
 )
 
+var separator = []byte{'\r', '\n', '\r', '\n'}
+
 func EncodeMessage(msg any) string {
 	content, err := json.Marshal(msg)
 	if err != nil {
 		panic(err)
 	}
 
-	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+	return fmt.Sprintf("Content-Length: %d%s%s", len(content), separator, content)
 }
 
 type BaseMessage struct {
@@ -22,13 +24,12 @@ type BaseMessage struct {
 }
 
 func DecodeMessage(msg []byte) (string, []byte, error) {
-	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	header, content, found := splitMessage(msg)
 	if !found {
 		return "", nil, errors.New("Did not find separator")
 	}
 
-	contentLengthBytes := header[len("Content-Length: "):]
-	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	contentLength, err := extractContentLength(header)
 	if err != nil {
 		return "", nil, err
 	}
@@ -40,4 +41,40 @@ func DecodeMessage(msg []byte) (string, []byte, error) {
 	}
 
 	return baseMessage.Method, actualContent, nil
+}
+
+// bufio scanner split implementation
+func Split(data []byte, _ bool) (advance int, token []byte, err error) {
+	header, content, found := splitMessage(data)
+	if !found {
+		// no error, just not ready to split yet
+		return 0, nil, nil
+	}
+
+	contentLength, err := extractContentLength(header)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	if len(content) < contentLength {
+		// also not ready
+		return 0, nil, nil
+	}
+
+	totalLength := len(header) + len(separator) + contentLength
+	return totalLength, data[:totalLength], nil
+}
+
+func splitMessage(data []byte) (header []byte, content []byte, found bool) {
+	return bytes.Cut(data, separator)
+}
+
+func extractContentLength(header []byte) (int, error) {
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	return contentLength, nil
 }
